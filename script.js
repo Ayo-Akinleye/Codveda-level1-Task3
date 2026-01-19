9c6ce94af53f20a7082a28da2c7c5985e0cf5783
MideJoy Akinleye <joyintech50@gmail.com>
2026-01-19 18:00:25 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+const displayNum = document.querySelector('.display-number')
+const increase = document.querySelector('.inc');
+const decrease = document.querySelector('.dec');
+const reset = document.querySelector('.reset');
+
+let count = 0;
+
+const updateDisplay = () => {
+    displayNum.textContent = count;
+};
+
+const handleIncrease = () => {
+    count++
+    updateDisplay()
+}
+
+const handleDecrease = () => {
+    count--
+    updateDisplay()
+}
+
+const handleReset = () => {
+    count = 0
+    updateDisplay()
+}
+
+increase.addEventListener('click', handleIncrease);
+decrease.addEventListener('click', handleDecrease);
+reset.addEventListener('click', handleReset);
